d2390765c161f6f8531a38b1349f1608848655a6
aaron592 <bsilent590@gmail.com>
2021-04-01 21:30:09 +0530

    bootstrap updates

--- adminpage.php
+++ adminpage.php
@@ -15,6 +15,15 @@ $result = mysqli_query($conn,$query);
   ?>
 
 <html>
+<head>
+<title>Admin Panel</title>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+</head>
+<body>
 
 <table class="table table-bordered">
   <thead>
