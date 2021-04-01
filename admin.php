0aba233ddbaa87796261287a711d51b8bf16d3fb
aaron592 <bsilent590@gmail.com>
2021-04-01 21:25:36 +0530

    bootstrap updates

--- admin.php
+++ admin.php
@@ -18,6 +18,12 @@ if(mysqli_num_rows($result)==1){
 }
  ?>
 <html><head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
 <title>Bible Study - Login</title>
     <link rel="shortcut icon" href="imgs/logo.png" type="image/x-icon">
     <!-- Bootstrap Core CSS -->
