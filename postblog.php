3b89b4b4e69727799391f233d4119dfd518d0e52
aaron592 <bsilent590@gmail.com>
2021-04-01 21:32:20 +0530

    bootstrap updates

--- postblog.php
+++ postblog.php
@@ -37,6 +37,11 @@ if(isset($_POST['submit'])){
 ?>
 <html>
 <head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 </head>
 <body>
 
